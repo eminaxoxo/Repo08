5f267fdff93552ae4f3e664f77bf99410e5f7eed
eminaxoxo <100778616+eminaxoxo@users.noreply.github.com>
2022-04-05 17:28:49 +0200

    Commit

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,11 +1,8 @@
-//write code below this line 
-function functionPrintInfo (name,age){
-    console.log (name,age);
-}
-functionPrintInfo ('John','23');
-functionPrintInfo ('Michael', '43');
-//write code above this line 
-
-module.exports = functionPrintInfo;
-
-//ovo je tacno 
+function functionPrintInfo(name,age){
+    console.log(name,age);
+    }
+    
+    functionPrintInfo("John", 23);
+    functionPrintInfo("Michael", 43);
+    
+    module.exports = functionPrintInfo;
